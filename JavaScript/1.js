98788d620322a6e23d6d789b93c4a2a640e5ea05
heyfenny <61340168+heyfenny@users.noreply.github.com>
2025-01-08 20:36:57 +0800

    Update 1.js

--- JavaScript/1.js
+++ JavaScript/1.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: true,
+        fade: true,
+        cssEase: 'linear
+    });
+});
